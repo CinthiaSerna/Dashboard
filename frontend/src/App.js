3cfc0fa2cc9dd75a07965370ffa769397ecd9872
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2025-03-15 17:48:24 -0500

    Funcionalidades de crear, editar y eliminar una tarea

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
+
+  const token = localStorage.getItem("token");
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/" />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Dashboard = () => {
+
+  const [tareas, setTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState("");
+
+  const token = localStorage.getItem("token"); 
+
+  useEffect(() => {
+    obtenerTareas();
+  }, []);
+
+  const obtenerTareas = async () =>{
+    try {
+      const response = await axios.get("http://localhost:5000/tareas", {
+        headers: {Authorization: `Bearer ${token}`}
+      });
+      setTareas(response.data);
+    } catch (error) {
+      console.error("Error al obtener las tareas:", error);
+    }
+  }
+
+// ---------------------------------------------------------------------------
+
+  const agregarTarea = async () =>{
+    if(!nuevaTarea.trim()) return;
+    try {
+      const response = await axios.post("http://localhost:5000/tareas", 
+        { texto: nuevaTarea },
+        { headers: {Authorization: `Bearer ${token}`} }
+      );
+      setTareas([...tareas, response.data]);
+      setNuevaTarea("");
+    } catch (error) {
+      console.error("Error al agregar la nueva tarea:" ,error);
+    }
+  }
+
+// ---------------------------------------------------------------------------
+
+  const editarTarea = async (id, nuevoTexto) => {
+    try {
+      const response = await axios.put(`http://localhost:5000/tareas/${id}`, 
+        { texto: nuevoTexto },
+        { headers : { Authorization: `Bearer ${token}` } }
+      );
+      setTareas(tareas.map((tarea) => (tarea.id === id ? response.data : tarea)));
+    } catch (error) {
+      console.error("Error al editar la tarea:", error);
+    }
+  }
+
+// ---------------------------------------------------------------------------
+
+  const eliminarTarea = async (id) =>{
+    try {
+        await axios.delete(`http://localhost:5000/tareas/${id}`, {
+        headers: { Authorization: `Bearer ${token}`},
+      });
+      setTareas(tareas.filter((tarea) => tarea.id !== id))
+    } catch (error) {
+      console.error("Error al eliminar la tarea:" , error)
+    }
+  }
+
   return (
-    <div> Futuro Dashboard </div>
+    <div>
+      <h1>Dashboard</h1>
+      <input 
+      type="text"
+      value={nuevaTarea}
+      onChange={(e) => setNuevaTarea(e.target.value)}
+      placeholder="Que tareas hay para hoy?"/>
+      <button onClick={agregarTarea}>Agregar</button>
+      <ul>
+        {tareas.map((tarea) => (
+          <li key={tarea.id}>
+            {tarea.texto}
+            <button onClick={() => eliminarTarea(tarea.id)}>Eliminar</button>
+            <button onClick={() => {
+              const nuevoTexto = prompt("Editar tarea", tarea.texto);
+              if(nuevoTexto) {
+                editarTarea(tarea.id, nuevoTexto);
+              }
+            }}>Editar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
